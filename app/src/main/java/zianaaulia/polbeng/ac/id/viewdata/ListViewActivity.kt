ef3d786b8a7e15d1fe7faa87869528b60499b2fa
Ziana Aulia <zianaaulia2602@gmail.com>
2022-05-25 22:48:15 +0700

    Listview

---
+++ app/src/main/java/zianaaulia/polbeng/ac/id/viewdata/ListViewActivity.kt
@@ -0,0 +1,21 @@
+package zianaaulia.polbeng.ac.id.viewdata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_list_view.*
+import java.util.*
+
+class ListViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_view)
+        var dataArray: Array<String> =
+            resources.getStringArray(R.array.jurusan)
+        Arrays.sort(dataArray)
+        var adapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1, dataArray)
+        listview.adapter = adapter
+
+    }
+}
---
+++ app/src/main/java/zianaaulia/polbeng/ac/id/viewdata/MainActivity.kt
@@ -0,0 +1,18 @@
+package zianaaulia.polbeng.ac.id.viewdata
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnlistview.setOnClickListener{
+            actionPindah(ListViewActivity())
+        }
+    }
+    private fun actionPindah(simpleActivity: Any) {
+        startActivity(Intent(applicationContext,simpleActivity::class.java))
+    }
+}
